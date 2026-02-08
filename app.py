e0a8e15dee065cc1639d1e8a051e17b6612734c9
SARAVANAN S <2025ab05112@wilp.bits-pilani.ac.in>
2026-02-08 12:55:36 +0530

    Model evaluation

--- app.py
+++ app.py
@@ -2,26 +2,7 @@ import streamlit as st
 import pandas as pd 
 import matplotlib.pyplot as plt 
 import seaborn as sns 
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import (
-    confusion_matrix,
-    classification_report,
-    accuracy_score,
-    precision_score,
-    recall_score,
-    f1_score
-)
-
-from sklearn.linear_model import LogisticRegression
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.naive_bayes import GaussianNB
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.multiclass import OneVsRestClassifier
-from xgboost import XGBClassifier
-
-
+from model.util import get_results
 # Streamlit Page 
 st.set_page_config(page_title="ML Assingment 2", layout="wide")
 st.title("Classification Application")
@@ -37,7 +18,7 @@ Application flow:
 # Upload Dataset
 uploaded_file = st.file_uploader("Upload CSV Dataset", type=["csv"])
 
- if uploaded_file is not None:
+if uploaded_file is not None:
     data = pd.read_csv(uploaded_file)
 
     st.subheader("Dataset Preview")
@@ -47,50 +28,40 @@ uploaded_file = st.file_uploader("Upload CSV Dataset", type=["csv"])
         st.error("Dataset must contain a 'fetal_health' target column.")
         st.stop()
 
-    X = data.drop(columns=["fetal_health"])
-    # Data Preprocessing 
-    # Convert 1 -> 0, 2 -> 1 and 3 -> 2
-    y = data["fetal_health"] - 1
-
-    # Split & Scaling
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y,
-        test_size=0.2,
-        random_state=42,
-        stratify=y
-    )
-
-    scaler = StandardScaler()
-    X_train_scaled = scaler.fit_transform(X_train)
-    X_test_scaled = scaler.transform(X_test)
-
+    
     # Model Selection
-    model_dict = {
-        "Logistic Regression ": OneVsRestClassifier(
-            LogisticRegression(max_iter=1000)
-        ),
-        "Decision Tree": DecisionTreeClassifier(random_state=42),
-        "K-Nearest Neighbors": KNeighborsClassifier(n_neighbors=5),
-        "Gaussian Naive Bayes": GaussianNB(),
-        "Random Forest": RandomForestClassifier(
-            n_estimators=100, random_state=42
-        ),
-        "XGBoost": XGBClassifier(
-            objective="multi:softprob",
-            num_class=3,
-            eval_metric="mlogloss",
-            random_state=42
-        )
-    }
+    model_dict = [
+        "Logistic Regression",
+        "Decision Tree",
+        "K-Nearest Neighbors",
+        "Gaussian Naive Bayes",
+        "Random Forest",
+        "XGBoost"
+    ]
 
-    selected_options = st.multiselect("Select Classification Model's", model_dict.keys())
-    #st.write("You selected:", model_names)
-    #model = model_dict[model_name]
+    selected_options = st.multiselect("Select Classification Model's", model_dict)
     
     if selected_options:
-        st.write("You selected the following options:")
+        st.subheader("Evaluation Metrics")
+        results = get_results(selected_options, data)
+
+        results_df = pd.DataFrame(results, index=["Accuracy", "AUC", "Precision", "Recall", "F1 Score", "MCC"]).T.round(4)
+
+        st.table(results_df)
         # Iterate over the list of selected options
-        for model in selected_options:
-            st.write(f"- {model}")
+        # for model_name in selected_options:
+        #     model = model_dict[model_name]
+        #     y_pred, y_prob, y_test_bin = train_model(model, X_train_scaled, y_train, X_test_scaled, y_test)
+        #     metrics = evalualte_metric(y_test, y_pred, y_prob, y_test_bin)
+        #     #st.write(metrics)
+        #     col1, col2, col3, col4, col5, col6, col7 = st.columns(6)
+        #     col1.metric("Accuracy", f"{metrics['accuracy']:.4f}")
+        #     col1.metric("Accuracy", f"{metrics['accuracy']:.4f}")
+        #     col2.metric("Precision", f"{metrics['precision']:.4f}")
+        #     col3.metric("Recall", f"{metrics['recall']:.4f}")
+        #     col4.metric("F1 Score", f"{metrics['f1']:.4f}")
+        #     col5.metric("AUC", f"{metrics['auc']:.4f}")
+        #     col6.metric("MCC", f"{metrics['mcc']:.4f}")
+
 
 
---
+++ model/util.py
@@ -0,0 +1,91 @@
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.multiclass import OneVsRestClassifier
+from xgboost import XGBClassifier
+
+from sklearn.preprocessing import label_binarize
+from sklearn.metrics import (
+    confusion_matrix,
+    classification_report,
+    accuracy_score,
+    precision_score,
+    recall_score,
+    f1_score,
+    roc_auc_score,
+    matthews_corrcoef
+)
+
+def get_model(model_name):
+    if model_name == "Logistic Regression":
+        return OneVsRestClassifier(
+            LogisticRegression(max_iter=1000)
+        )
+    elif model_name == "Decision Tree":
+        return DecisionTreeClassifier(random_state=42)
+    elif model_name == "K-Nearest Neighbors":
+        return  KNeighborsClassifier(n_neighbors=5)
+    elif model_name == "Gaussian Naive Bayes":
+        return GaussianNB()
+    elif model_name == "Random Forest":
+        return RandomForestClassifier(
+            n_estimators=100, random_state=42
+        )
+    elif model_name == "XGBoost":
+        return XGBClassifier(
+            objective="multi:softprob",
+            num_class=3,
+            eval_metric="mlogloss",
+            random_state=42
+        )
+
+
+def evalualte_model(model_name, data):
+
+    X = data.drop(columns=["fetal_health"])
+    # Data Preprocessing 
+    # Convert 1 -> 0, 2 -> 1 and 3 -> 2
+    y = data["fetal_health"] - 1
+
+    # Split & Scaling
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y,
+        test_size=0.2,
+        random_state=42,
+        stratify=y
+    )
+
+    #Scaling
+    scaler = StandardScaler()
+    X_train_scaled = scaler.fit_transform(X_train)
+    X_test_scaled = scaler.transform(X_test)
+
+
+    # Model Selection
+    model = get_model(model_name)
+    model.fit(X_train_scaled, y_train)
+    
+    # Model Prediction
+    y_pred = model.predict(X_test_scaled)
+    y_prob = model.predict_proba(X_test_scaled)
+    y_test_bin = label_binarize(y_test, classes=[0,1,2])
+
+    return {
+        "Accuracy":accuracy_score(y_test, y_pred),
+        "AUC":roc_auc_score(y_test_bin, y_prob, multi_class="ovr"),
+        "Precision":precision_score(y_test, y_pred, average="weighted"),
+        "Recall":recall_score(y_test, y_pred, average="weighted"),
+        "F1 Score":f1_score(y_test, y_pred, average="weighted"),
+        "MCC":matthews_corrcoef(y_test, y_pred)
+    }
+
+def get_results(models, data):
+    results = {
+        model_name: evalualte_model(model_name, data) for model_name in models
+    }
+    return results
